a80abc759a9b318ea4c7e3767b1f48eb181c950c
Reshmavijayakumar <52028018+Reshmavijayakumar@users.noreply.github.com>
2021-12-21 19:24:06 +0530

    Add files via upload

---
+++ tweetapp/src/main/java/com/tweetapp/model/Post.java
@@ -0,0 +1,28 @@
+package com.tweetapp.model;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.tweetapp.constants.ServiceConstants;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Document(collection = ServiceConstants.POST_COLLECTION_TABLE)
+public class Post {
+	public Post() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Id
+	String id;
+	String author;
+	String postMessage;
+	String hasTag;
+	Date dateOfPost;
+	Long likesCount;
+}
